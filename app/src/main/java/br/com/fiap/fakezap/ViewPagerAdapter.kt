5603e4973ba937c513e8f4124212594dcc7621d0
logonrm <logonrm@fiap.com.br>
2018-03-19 23:01:13 -0300

    Salvando ...

---
+++ app/src/main/java/br/com/fiap/fakezap/ViewPagerAdapter.kt
@@ -0,0 +1,23 @@
+package br.com.fiap.fakezap
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+
+    val meusFragments = ArrayList<Fragment>()
+
+    override fun getItem(position: Int): Fragment {
+        return meusFragments[position]
+    }
+
+    override fun getCount(): Int {
+        return meusFragments.size
+    }
+
+    fun adicionar(fragment : Fragment) {
+        meusFragments.add(fragment)
+    }
+
+}
